b9b1ac55f5ba6b1b1ef2f22a7d84f9deab96ff8d
Kaptian Core <kaptiancore@gmail.com>
2020-11-12 10:36:03 +1100

    All This

---
+++ ggs.js
@@ -0,0 +1,50 @@
+
+var ggsjs = function(){};
+
+let axios = requoire("axios")
+
+// e.g let data = ggs.discordQuery(token, discordid);
+ggsjs.discordQuery = function(token, discordid) {
+    try {
+        let response = await axios.get(`https://ggs.sx/api/v1/user/discord/${discordid}?api_token=${token}`)
+        return response;
+        }catch(error) {
+            let current_date = moment().format('HH:mm | DD/MM/YYYY')
+            return (`${current_date} Error With Function discordQuery`, error)
+        };
+};
+
+// e.g let data = ggs.steamQuery(token, steamid);
+ggsjs.steamQuery = function(token, steamid) {
+    try {
+        let response = await axios.get(`https://ggs.sx/api/v1/user/steam/${steamid}?api_token=${token}`)
+        return response;
+        }catch(error) {
+            let current_date = moment().format('HH:mm | DD/MM/YYYY')
+            return (`${current_date} Error With Function steamQuery`, error)
+        };
+};
+
+// e.g let data = ggs.serversQuery();
+ggsjs.serversQuery = function() {
+    try {
+        let response = await axios.get('https://ggs.sx/api/v1/servers')
+        return response;
+    }catch (error) {
+        let current_date = moment().format('HH:mm | DD/MM/YYYY')
+        return (`${current_date} Error With Function serversQuery`, error)
+    }
+}
+
+// e.g let data = ggs.shoutsQuery();
+ggsjs.shoutsQuery = function() {
+    try {
+        let response = await axios.get('https://ggs.sx/api/v1/shouts/')
+        return response;
+    }catch (error) {
+        let current_date = moment().format('HH:mm | DD/MM/YYYY')
+        return (`${current_date} Error With Function shoutsQuery`, error)
+    }
+}
+
+module.exports = new ggsjs();
